2053c5148b884bc22765cd59d1dfce1c4a461b56
wellingtonctm <w.luiscm@gmail.com>
2021-09-12 19:00:22 -0300

    Primeira versão front-end da página de início.

--- header.php
+++ header.php
@@ -1,10 +1,20 @@
 <header>
-    <div class="container"> 
-        <nav class="navbar navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" style="font-family: Pacifico;">Pizzaria do Luigi</a>
-                <button class="btn btn-primary">escolher</button>
+    <nav class="navbar navbar-expand-lg d-block shadow-bottom p-0">
+        <div class="container-fluid bg-yellow py-1 px-3">
+            <a href="index.php">
+                <img src="Img/luigi-logo.png" height="60px"/>
+            </a>
+            <button class="btn btn-red">Faça seu pedido</button>
+        </div>
+        <div class="container-fluid bg-white navbar-light">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Cardápio</a>
+                    <a class="nav-link" href="#">Minha Conta</a>
+                    <a class="nav-link" href="#">Fale conosco</a>
+                    <a class="nav-link" href="#">Sobre nós</a>
+                </div>
             </div>
-        </nav>
-    </div>
+        </div>
+    </nav>
 </header>
--- index.php
+++ index.php
@@ -1,17 +1,24 @@
+<!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
         <title> Pizzaria do Luigi </title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/Pacifico.css" rel="stylesheet">
+        <link href="css/pacifico.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/carousel.css" rel="stylesheet">
+        <link href="css/navbar.css" rel="stylesheet">
     </head>
-    <body>
+    <body class="d-flex flex-column h-100">
 
-    <?php
-        include 'header.php';
-        include 'body.php';
-        include 'footer.php';
-    ?>
+        <?php
+            include 'header.php';
+            include 'body.php';
+            include 'footer.php';
+        ?>
 
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-</body>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.min.js"></script>
+        <script src="js/popper.min.js"></script>
+    </body>
+</html>
