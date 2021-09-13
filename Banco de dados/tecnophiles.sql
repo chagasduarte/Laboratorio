564bb581c954e64ba6efd15d759c572a17284241
Chagas Duarte <chagasduarte.rdg@gmail.com>
2021-09-13 13:40:20 -0300

    Adicionado tabelas de produtos, pedidos e clientes

--- Banco de dados/tecnophiles.sql
+++ Banco de dados/tecnophiles.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 09-Set-2021 às 00:31
+-- Tempo de geração: 13-Set-2021 às 18:38
 -- Versão do servidor: 10.4.21-MariaDB
 -- versão do PHP: 8.0.10
 
@@ -20,6 +20,18 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `tecnophiles`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `Id_Cliente` int(11) NOT NULL,
+  `Nome` varchar(25) NOT NULL,
+  `Data_Inscricao` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -41,15 +53,68 @@ CREATE TABLE `mesas` (
 INSERT INTO `mesas` (`Id`, `Id_Garcom`, `Status`, `Id_Pedidos`) VALUES
 (1, 1, 1, 1);
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `Id_Pedidos` int(11) NOT NULL,
+  `Id_Produto` int(11) NOT NULL,
+  `Qtd_Prod` int(11) NOT NULL,
+  `Preco_Total` float(10,0) NOT NULL,
+  `Data_Hora_Ped` datetime NOT NULL,
+  `Id_Cliente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `pedidos`
+--
+
+INSERT INTO `pedidos` (`Id_Pedidos`, `Id_Produto`, `Qtd_Prod`, `Preco_Total`, `Data_Hora_Ped`, `Id_Cliente`) VALUES
+(1, 1, 1, 1000, '0000-00-00 00:00:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `Id_Prod` int(11) NOT NULL,
+  `Descricao` varchar(25) NOT NULL,
+  `Preco` float(10,0) NOT NULL,
+  `Qtd_Estoque` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`Id_Cliente`);
 
 --
 -- Índices para tabela `mesas`
 --
 ALTER TABLE `mesas`
   ADD PRIMARY KEY (`Id`);
+
+--
+-- Índices para tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`Id_Pedidos`);
+
+--
+-- Índices para tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`Id_Prod`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
