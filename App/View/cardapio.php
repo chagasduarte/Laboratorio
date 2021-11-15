e00888bdcedb604965e6b497f05bee587f882028
wellingtonctm <w.luiscm@gmail.com>
2021-11-15 19:25:11 -0300

    Criação da view de cardápio.

---
+++ App/View/cardapio.php
@@ -0,0 +1,14 @@
+<?php include('Templates/pre_def.php') ?>
+
+<!DOCTYPE html>
+<html class="h-100">
+	<?php include('Templates/html-head.php') ?>
+	<body class="d-flex flex-column h-100">
+		<?php include('Templates/header.php') ?>
+		<div class="container-fluid col-lg-10 p-0">
+		
+		</div>
+  		<?php include('Templates/footer.php') ?>
+		<?php include('Templates/html-script.php') ?>
+	</body>
+</html>
