1794a79c763fa223584a08d0e78de8a04bffc784
wellingtonctm <w.luiscm@gmail.com>
2021-11-22 18:39:53 -0300

    Organização da visualização da página inicial.

---
+++ App/View/index/admin.php
@@ -0,0 +1,39 @@
+		<div class="container col-7">
+			<div class="container-fluid d-flex justify-content-between">
+				<a class="card bg-yellow text-center p-3 fg-red text-decoration-none">
+					<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+					  <path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+					  <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+					</svg>
+					Pagina Inicial
+				</a>
+
+				<a class="card bg-yellow text-center p-3 fg-red text-decoration-none">
+					<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+					  <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+					  <path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/>
+					  <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
+					</svg>
+					Funcionários
+				</a>
+
+				<a class="card bg-yellow text-center p-3 fg-red text-decoration-none">
+					<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+					  <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+					  <path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/>
+					  <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
+					</svg>
+					Clientes
+				</a>
+			</div>
+
+			<div class="container-fluid d-flex justify-content-between">
+				<a class="card bg-yellow text-center p-3 fg-red text-decoration-none" href="ingredientes/listar.php">
+					<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
+					  <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+					  <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+					</svg>
+					Estoque
+				</a>
+			</div>
+		</div>
---
+++ App/View/index/cliente.php
@@ -0,0 +1,86 @@
+		<div id="carouselExampleCaptions" class="carousel slide mt-5 mb-5" data-bs-ride="carousel">
+    		<div class="carousel-indicators">
+        		<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        		<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        		<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    		</div>
+    		<div class="carousel-container">
+        		<div class="carousel-inner">
+            		<div class="carousel-item active">
+                		<img src="<?php echo $img ?>/carrossel1.jpg" class="img-fluid carousel-img" alt="">
+                		<div class="carousel-caption">
+                    		<h5>First slide label</h5>
+                    		<p>Some representative placeholder content for the first slide.</p>
+                		</div>
+            		</div>
+            		<div class="carousel-item">
+                		<img src="<?php echo $img ?>/carrossel2.jpg" class="img-fluid carousel-img" alt="">
+                		<div class="carousel-caption">
+                    		<h5>Second slide label</h5>
+                    		<p>Some representative placeholder content for the second slide.</p>
+                		</div>
+            		</div>
+            		<div class="carousel-item">
+                		<img src="<?php echo $img ?>/carrossel3.jpg" class="img-fluid carousel-img" alt="">
+                		<div class="carousel-caption">
+                    		<h5>Third slide label</h5>
+                    		<p>Some representative placeholder content for the third slide.</p>
+                		</div>
+            		</div>
+        		</div>
+    		</div>
+
+    		<a class="carousel-control-prev d-none d-lg-flex" href="#carouselExampleCaptions" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        		<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+            		<path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+        		</svg>
+    		</a>
+    		<a class="carousel-control-next d-none d-lg-flex" href="#carouselExampleCaptions" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        		<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+            		<path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+        		</svg>
+    		</a>
+		</div>
+
+
+		<div class="container">
+    		<div class="justify-content-center row">
+        		<div class="bg-yellow card card-body col-5 p-3 align-items-end">
+            		<img src="<?php echo $img ?>/card1.jpg" class="card-img p-0 mb-3 col-4">
+            		<div class="card-txt fg-red">
+                		<h4>Promoção:<br>
+                		Combo família!</h4>
+            		</div>
+            		<button class="btn btn-green float-right">Peça agora!</button>
+        		</div>
+        		<div class="bg-yellow card card-body col-5 p-3 align-items-end">
+            		<img src="<?php echo $img ?>/card1.jpg" class="card-img p-0 mb-3 col-4">
+            		<div class="card-txt fg-red">
+                		<h4>Promoção:<br>
+                		Combo dia dos namorados!</h4>
+            		</div>
+            		<button class="btn btn-green float-right">Peça agora!</button>
+        		</div>
+    		</div>
+		</div>
+
+		<div class="container">
+    		<div class="justify-content-center row">
+        		<div class="bg-yellow card card-body col-5 p-3 align-items-end">
+            		<img src="<?php echo $img ?>/card1.jpg" class="card-img p-0 mb-3 col-4">
+            		<div class="card-txt fg-red">
+                		<h4>Promoção:<br>
+                		Combo família!</h4>
+            		</div>
+            		<button class="btn btn-green float-right">Peça agora!</button>
+        		</div>
+        		<div class="bg-yellow card card-body col-5 p-3 align-items-end">
+            		<img src="<?php echo $img ?>/card1.jpg" class="card-img p-0 mb-3 col-4">
+            		<div class="card-txt fg-red">
+                		<h4>Promoção:<br>
+                		Combo dia dos namorados!</h4>
+            		</div>
+            		<button class="btn btn-green float-right">Peça agora!</button>
+        		</div>
+    		</div>
+		</div>
